736b01b7049df9f062f1832b85bd6528bd5744ad
Sanjay Chhikara <sanjaychhikara77@gmail.com>
2023-12-03 20:38:03 +0530

    react added

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",{id:"heading"},"hello world from iside react");
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},heading))
+
+
+const root =  ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+console.log(heading);
