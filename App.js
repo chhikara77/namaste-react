272dc867e0ec233178c20f1d1a8fcb9e32dead8a
Sanjay Chhikara <sanjaychhikara77@gmail.com>
2023-12-07 20:01:19 +0530

    usig jsx

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1",{id:"heading"},"hello world from iside react");
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "hello worlds from iside react"
+    );
 
-
+const jsxheading = <h1 id="heading">this is jsx heading</h1>
 
 const root =  ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-console.log(parent);
+root.render(jsxheading);
+console.log(heading);
+console.log(jsxheading); //both connsole return same object
